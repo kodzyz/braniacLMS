680c3da08ffb2e4a93ba5629c6ef4117f863bb52
ko_dzy <kodzyzdok@gmail.com>
2022-05-24 23:38:27 +0300

    task_4_1 Поля связи BaseModel, Course, Lesson, CoursesTeachers

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,25 +1,17 @@
 from django.db import models
 
+NULLABLE = {'blank': True, 'null': True}
 
 # Create your models here.
 
-class News(models.Model):
-    title = models.CharField(max_length=255, verbose_name='Заголовок')
-    preamble = models.CharField(max_length=1000, verbose_name='Вступление')
-    body = models.TextField(verbose_name='Содержимое')
-    body_as_markdown = models.BooleanField(default=False, verbose_name='Способ разметки')
-
+class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
 
     deleted = models.BooleanField(default=False, verbose_name='Удален')
 
-    def __str__(self):
-        return f'#{self.pk} {self.title}'
-
     class Meta:
-        verbose_name = 'новость'
-        verbose_name_plural = 'новости'
+        abstract = True
         ordering = ('-created_at',)
 
     def delete(self, *args, **kwargs):
@@ -27,3 +19,73 @@ class News(models.Model):
         self.save()
 
 
+class News(BaseModel):
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    preamble = models.CharField(max_length=1000, verbose_name='Вступление')
+    body = models.TextField(verbose_name='Содержимое')
+    body_as_markdown = models.BooleanField(default=False, verbose_name='Способ разметки')
+
+    def __str__(self):
+        return f'#{self.pk} {self.title}'
+
+    class Meta:
+        verbose_name = 'новость'
+        verbose_name_plural = 'новости'
+
+
+# Поля связи
+
+class Course(BaseModel):
+    name = models.CharField(max_length=256, verbose_name="Name")
+    description = models.TextField(verbose_name="Description", **NULLABLE)  # ** -> распаковка
+    description_as_markdown = models.BooleanField(verbose_name="As markdown", default=False)
+    cost = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Cost", default=0)
+    cover = models.CharField(max_length=25, default="no_image.svg", verbose_name="Cover")
+
+    def __str__(self) -> str:
+        return f"{self.pk} {self.name}"
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
+
+class Lesson(BaseModel):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)  # Связь «один ко многим»
+    num = models.PositiveIntegerField(verbose_name="Lesson number")
+    title = models.CharField(max_length=256, verbose_name="Name")
+    description = models.TextField(verbose_name="Description", **NULLABLE)
+    description_as_markdown = models.BooleanField(verbose_name="As markdown", default=False)
+
+    def __str__(self) -> str:
+        return f"{self.course.name} | {self.num} | {self.title}"
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
+        ordering = ("course", "num")
+
+
+class CoursesTeachers(BaseModel):
+    courses = models.ManyToManyField(Course)  # Связь «многие ко многим»
+    name_first = models.CharField(max_length=128, verbose_name="Name")
+    name_second = models.CharField(max_length=128, verbose_name="Surname")
+    day_birth = models.DateField(verbose_name="Birth date")
+
+    def __str__(self) -> str:
+        return "{0:0>3} {1} {2}".format(self.pk, self.name_second, self.name_first)
+
+# миграции - внесение изм в БД в соответствии с определениями в модели
+
+# шаг 1: создаем описание полей
+# в терминале:
+# -> python manage.py makemigrations
+
+# шаг 2: создать таблицу в БД
+# -> python manage.py migrate
+
+#  пустая миграция :
+# -> python manage.py makemigrations --empty --name data_load mainapp
+
+# -> pip install ipython
+# -> python manage.py shell
