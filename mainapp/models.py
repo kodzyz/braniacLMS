a26ddfec3308a335a7388d9f0decdca94e48b89f
ko_dzy <kodzyzdok@gmail.com>
2022-05-24 22:01:13 +0300

    task_4_1 Создайте модели для новостей, курсов, уроков и преподавателей. Используйте различные типы полей

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+
 # Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    preamble = models.CharField(max_length=1000, verbose_name='Вступление')
+    body = models.TextField(verbose_name='Содержимое')
+    body_as_markdown = models.BooleanField(default=False, verbose_name='Способ разметки')
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+
+    deleted = models.BooleanField(default=False, verbose_name='Удален')
+
+    def __str__(self):
+        return f'#{self.pk} {self.title}'
+
+    class Meta:
+        verbose_name = 'новость'
+        verbose_name_plural = 'новости'
+        ordering = ('-created_at',)
+
+    def delete(self, *args, **kwargs):
+        self.deleted = True
+        self.save()
+
+
