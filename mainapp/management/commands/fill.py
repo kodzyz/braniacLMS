e0a7e0e2021b22d180b4cc4d171ea3ff0eaccb84
ko_dzy <kodzyzdok@gmail.com>
2022-05-25 23:39:29 +0300

    task_* command пакетная запись

--- mainapp/management/commands/fill.py
+++ mainapp/management/commands/fill.py
@@ -2,15 +2,16 @@ from django.core.management import BaseCommand
 
 from mainapp.models import News
 
+
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        #print('hello terminal')  # python manage.py fill -> hello terminal
+        news_list = []
         for i in range(10):
-            News.object.create(
+            news_list.append(News(
                 title=f'title#{i}',
                 preamble=f'preamble#{i}',
                 body='some body'
-            )
+            ))
 
-
+        News.object.bulk_create(news_list)  # пакетное создание
