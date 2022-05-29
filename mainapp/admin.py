2c342356680972787d98f0fbc7011b05182be04a
ko_dzy <kodzyzdok@gmail.com>
2022-05-29 20:46:42 +0300

    task_6_1 регистрация моделей News, Course, Lesson, CoursesTeachers

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from mainapp.models import News, Course, Lesson, CoursesTeachers
 
 # Register your models here.
+
+# Зарегистрируйте модель пользователя
+admin.site.register(News)
+admin.site.register(Course)
+admin.site.register(Lesson)
+admin.site.register(CoursesTeachers)
