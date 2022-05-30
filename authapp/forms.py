67ea6961e2ae46a4e2f78b8dd91a67bdd957902c
ko_dzy <kodzyzdok@gmail.com>
2022-05-30 22:53:51 +0300

    валидация в форме по полю

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model  # ссылка на актуальную модель пользователя
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-
+from django.core.exceptions import ValidationError
 
 # class StyleFormMixin:
 #
@@ -32,3 +32,9 @@ class CustomUserCreationForm(UserCreationForm):
             'age',
             'avatar'
         )
+
+    def clean_age(self):
+        age = self.cleaned_data.get('age')
+        if age < 18:
+            raise ValidationError('Вы слишком молоды для этого сайта')  # исключение
+        return age
