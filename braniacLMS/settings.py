42234b3467e90f4b4da8fa608d272c1820c2e65d
ko_dzy <kodzyzdok@gmail.com>
2022-06-04 22:15:52 +0300

    task_7_3 кеш-система на основе Redis

--- braniacLMS/settings.py
+++ braniacLMS/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import debug_toolbar.middleware
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
@@ -24,7 +26,12 @@ SECRET_KEY = 'django-insecure-nf%v&y$x65co32oy&u9zg4o2f^$6gc(dbyl)(7a2fx)0ro+e5&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True  # состояние отладки
 
-ALLOWED_HOSTS = []  # список хостов, адреса для защиты от кросформенных запросов
+ALLOWED_HOSTS = ["*"]  # список хостов, адреса для защиты от кросформенных запросов
+
+if DEBUG:
+    INTERNAL_IPS = [
+        "127.0.0.1",
+    ]
 
 # Application definition
 
@@ -36,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
-
+    'debug_toolbar',
     'social_django',
 
     'mainapp',
@@ -51,7 +58,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]  #
+    'debug_toolbar.middleware.DebugToolbarMiddleware'
+]
 
 ROOT_URLCONF = 'braniacLMS.urls'  # точка входа
 
@@ -147,26 +155,3 @@ SOCIAL_AUTH_GITHUB_KEY = 'cdd41f9830bed7587936'
 SOCIAL_AUTH_GITHUB_SECRET = 'b97beb832b9ce4643072b1cddee504901d33c401'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-
-LOG_FILE = BASE_DIR / "log" / "main_log.log"
-
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "console": {
-            "format": "[%(asctime)s] %(levelname)s %(name)s (%(lineno)d)%(message)s"},
-    },
-    "handlers": {
-        "file": {
-            "level": "INFO",
-            "class": "logging.FileHandler",
-            "filename": LOG_FILE,
-            "formatter": "console",
-        },
-        "console": {"class": "logging.StreamHandler", "formatter": "console"},
-    },
-    "loggers": {
-        "django": {"level": "INFO", "handlers": ["file", "console"]},
-    },
-}
