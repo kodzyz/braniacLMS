9da662ea31addbc58bfab73e6268c7a867f196ca
ko_dzy <kodzyzdok@gmail.com>
2022-06-06 22:02:25 +0300

    task_8_1 пагинатор в шаблоне новостей

--- mainapp/views.py
+++ mainapp/views.py
@@ -72,7 +72,7 @@ class LoginView(TemplateView):
 
 class NewsListView(ListView):
     model = News
-    paginate_by = 5
+    paginate_by = 2
 
     def get_queryset(self):
         return super().get_queryset().filter(deleted=False)
