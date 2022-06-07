243f4029dea3e6a060e50ce8eb1453f0808e6c69
ko_dzy <kodzyzdok@gmail.com>
2022-06-07 23:42:34 +0300

    task_8_3 тест на открытие страниц

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from http import HTTPStatus
 
 # Create your tests here.
+from django.urls import reverse
+
+
+class StaticPagesSmokeTest(TestCase):
+
+    def test_page_index_open(self):
+        url = reverse('mainapp:index')
+        result = self.client.get(url)
+
+        self.assertEqual(result.status_code, HTTPStatus.OK)  # 200 'Request fulfilled, document follows'
+
+    def test_page_contacts_open(self):
+        url = reverse('mainapp:contacts')
+        result = self.client.get(url)
+
+        self.assertEqual(result.status_code, HTTPStatus.OK)
+
+# python manage.py test
+# один метод -> python manage.py test mainapp.tests.StaticPagesSmokeTest.test_page_contacts_open
