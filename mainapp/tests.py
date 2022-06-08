2e5867e62ed05e8e6bd04397d10a5ac3f8970c10
ko_dzy <kodzyzdok@gmail.com>
2022-06-08 22:42:02 +0300

    task_8_3 тесты для проверки CRUD

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,8 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from http import HTTPStatus
 
 # Create your tests here.
 from django.urls import reverse
+
+from authapp.models import User
+from mainapp.models import News
 
 
 class StaticPagesSmokeTest(TestCase):
@@ -19,5 +22,51 @@ class StaticPagesSmokeTest(TestCase):
 
         self.assertEqual(result.status_code, HTTPStatus.OK)
 
-# python manage.py test
-# один метод -> python manage.py test mainapp.tests.StaticPagesSmokeTest.test_page_contacts_open
+
+class NewsTestCase(TestCase):  # функцональный тест
+
+    def setUp(self) -> None:  # выполнение команд перед тестом
+        # super().setUp()
+        for i in range(10):
+            News.object.create(  # -> заполнение бд
+                title=f'News{i}',
+                preamble=f'Intro{i}',
+                body=f'Body{i}'
+            )
+        # авторизованный клиент
+        User.objects.create_superuser(username='django', password='geekbrains')  # создаем автор. пользователя
+        self.client_with_auth = Client()
+        auth_url = reverse('authapp:login')  # адрес на авторизацию
+        self.client_with_auth.post(
+            auth_url,
+            {'username': 'django', 'password': 'geekbrains'}
+        )
+
+    def test_open_page(self):
+        url = reverse('mainapp:news')
+        result = self.client.get(url)
+
+        self.assertEqual(result.status_code, HTTPStatus.OK)
+
+    def test_failed_open_add_by_anonym(self):  # неудачное открытие неавторизованным страницы редактирования
+        url = reverse('mainapp:news_create')
+        result = self.client.get(url)
+
+        self.assertEqual(result.status_code, HTTPStatus.FOUND)  # 302
+
+    def test_create_news_item_by_admin(self):
+        news_count = News.object.all().count()
+
+        url = reverse('mainapp:news_create')
+        result = self.client_with_auth.post(
+            url,
+            data={
+                'title': 'Test news',
+                'preamble': 'Test intro',
+                'body': 'Test body'
+            }
+        )
+
+        self.assertEqual(result.status_code, HTTPStatus.FOUND)
+
+        self.assertEqual(News.object.all().count(), news_count + 1)
